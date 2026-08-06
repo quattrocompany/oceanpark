f463cbedd3bd52383288c33d7f68f4e7830ca24e
Quattro Company <marketing@quattroinc.com.br>
2026-08-06 15:35:30 -0300

    push

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,32 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'oceanosasco.com.br',
+          },
+        ],
+        destination: 'https://www.oceanosasco.com.br/:path*',
+        permanent: true,
+      },
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'oceanosasco.com.br',
+          },
+        ],
+        destination: 'https://www.oceanosasco.com.br/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
